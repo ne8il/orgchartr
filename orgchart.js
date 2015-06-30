cf179d4485ccee30ca4338457ea7079a4c362b4f
Neil Ahrendt <neil@salesvue.com>
2015-06-29 21:55:27 -0400

    changing to org chart stuff

--- orgchart.js
+++ orgchart.js
@@ -1,8 +1,18 @@
 Tasks = new Mongo.Collection("tasks");
+People = new Mongo.Collection("people");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("people");
+
   Template.body.helpers({
+
+    isAddingPerson : function(){
+      return Session.get("addingPerson");
+    },
+    people : function(){
+      return People.find({});
+    },
     tasks: function () {
       if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
@@ -21,15 +31,32 @@ if (Meteor.isClient) {
   });
 
   Template.body.events({
+
+  "click .add-person" : function(event){
+    Session.set("addingPerson", true);
+  },
+
+  "click .new-person button.cancel" : function(event){
+    Session.set("addingPerson", false);
+  },
+
+  "click .add-team" : function(event){
+
+  },
+
+  "submit .new-person" : function(event){
+    var name = event.target.text.value;
+    Meteor.call("addPerson", text);
+    event.target.text.value = "";
+    return false;
+  },
+
   "submit .new-task": function (event) {
       // This function is called when the new task form is submitted
 
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+      Meteor.call("addTask", text);
 
       // Clear form
       event.target.text.value = "";
@@ -46,10 +73,12 @@ if (Meteor.isClient) {
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked);
+
+
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
 }
@@ -77,4 +106,41 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.publish("people", function () {
+    return People.find();
+  });
 }
+
+Meteor.methods({
+
+  addPerson : function(text){
+    People.insert({
+      name: text,
+      createdAt: new Date()
+      //owner: Meteor.userId(),
+      //username: Meteor.user().username
+    });
+  },
+
+
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    //if (! Meteor.userId()) {
+    //  throw new Meteor.Error("not-authorized");
+    //}
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date()
+      //owner: Meteor.userId(),
+      //username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
